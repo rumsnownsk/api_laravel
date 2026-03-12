ee449dedc8d5dec2335e90769c41913dcbbd170b
rum <rumsnownsk@gmail.com>
2026-03-12 09:43:51 +0300

    fix cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*'], // Пути, к которым применяется CORS
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], // Пути, к которым применяется CORS
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Разрешённые HTTP-методы
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => ['https://api.iocode.ru', 'https://iocode.ru', 'http://localhost:3000', 'https://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
